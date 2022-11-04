69ff3694d5ecf0a53a5f17fe118b40f22dbbff34
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2022-11-04 09:11:13 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import { useEffect, useState } from "react";
+
+const Produtos = () => {
+  const [produtos, setProdutos] = useState([]);
+  useEffect(() => {
+    async function getProdutos() {
+      try {
+        const resposta = await fetch("https://fakestoreapi.com/products");
+        const dados = await resposta.json();
+
+        console.log(dados);
+        setProdutos(dados);
+      } catch (error) {
+        console.log("Deu ruim na busca post: " + error.message);
+      }
+    }
+    getProdutos();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="caixa">
+      <h2>Produtos</h2>
+      <ul>
+        {produtos.map(({ id, title, image, price }) => (
+          <li key={id}>
+            <p>
+              <img class="foto-produto" src={image} alt="" />
+            </p>
+            <div className="descricao">
+              <h2>{title}</h2>
+              <p>
+                {price.toLocaleString("pt-br", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default Produtos;
